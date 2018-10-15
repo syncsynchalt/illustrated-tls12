b84a82f0622c402a5f2bffd589af802fe5307c90
Michael Driscoll <fenris@ulfheim.net>
2018-10-15 15:16:56 -0600

    Set up a jshintrc and convert to good-parts form

--- site/illustrated.js
+++ site/illustrated.js
@@ -1,29 +1,33 @@
-ill = {
+(function () {
+	"use strict";
+
+	let ill = {};
+
 	// viewports etc
 
-	elementIsVisible: function(el) {
-		var rect = el.getBoundingClientRect(),
+	ill.elementIsVisible = function(el) {
+		let rect = el.getBoundingClientRect(),
 			viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
 		return !(rect.bottom < 0 || rect.top - viewHeight >= 0);
-	},
+	};
 
-	ensureElementInView: function(el) {
+	ill.ensureElementInView = function(el) {
 		if (!ill.elementIsVisible(el)) {
-			el.scrollIntoView({behavior: 'smooth'});
+			el.scrollIntoView({behavior: "smooth"});
 		}
-	},
+	};
 
 	// events
 
-	unselectAllRecords: function() {
+	ill.unselectAllRecords = function() {
 		[].forEach.call(document.querySelectorAll(".illustrated .record.selected, .illustrated .calculation.selected"),
 		function(el) {
 			el.classList.remove("selected");
 		});
-	},
+	};
 
-	toggleRecord: function(element, event) {
-		var selected = element.classList.contains("selected");
+	ill.toggleRecord = function(element, event) {
+		let selected = element.classList.contains("selected");
 		ill.unselectAllRecords();
 		if (!selected) {
 			element.classList.add("selected");
@@ -32,59 +36,59 @@ ill = {
 		}
 		if (event) { event.stopPropagation(); }
 		ill.ensureElementInView(element);
-	},
+	};
 
-	selectRecord: function(element, event) {
+	ill.selectRecord = function(element, event) {
 		ill.unselectAllRecords();
 		element.classList.add("selected");
 		if (event) { event.stopPropagation(); }
 		ill.ensureElementInView(element);
-	},
+	};
 
-	showCode: function(element, event) {
+	ill.showCode = function(element, event) {
 		element.parentNode.classList.add("show");
 		if (event) { event.stopPropagation(); }
-	},
+	};
 
-	closeAllCode: function() {
+	ill.closeAllCode = function() {
 		[].forEach.call(document.querySelectorAll("codesample.show"), function(el) {
 			el.classList.remove("show");
 		});
-	},
+	};
 
-	toggleAnnotate: function(el) {
+	ill.toggleAnnotate = function(el) {
 		el.classList.toggle("annotate");
-	},
+	};
 
-	cancel: function(event) {
+	ill.cancel = function(event) {
 		if (event) { event.stopPropagation(); }
-	},
+	};
 
 	// injections
 
-	addShowCode: function(el) {
-		el.innerHTML = document.getElementById('showCodeTmpl').innerHTML + el.innerHTML;
-	},
+	ill.addShowCode = function(el) {
+		el.innerHTML = document.getElementById("showCodeTmpl").innerHTML + el.innerHTML;
+	};
 
-	addToggleAnnotations: function(record) {
-		var expl = record.querySelector(".explanation"),
+	ill.addToggleAnnotations = function(record) {
+		let expl = record.querySelector(".explanation"),
 			copy = document.getElementById("annotateTmpl").cloneNode(true);
 		expl.insertAdjacentElement("afterend", copy);
-	},
+	};
 
-	injectLabels: function() {
-		var els = document.querySelectorAll(".string > .explanation, .decryption > .explanation");
+	ill.injectLabels = function() {
+		let els = document.querySelectorAll(".string > .explanation, .decryption > .explanation");
 		[].forEach.call(els, function(expl) {
-			var label = expl.parentNode.querySelector(".label"),
+			let label = expl.parentNode.querySelector(".label"),
 				h4 = document.createElement("h4");
 			h4.appendChild(document.createTextNode(label.textContent));
 			expl.insertAdjacentElement("afterbegin", h4);
 		});
-	},
+	};
 
-	printMode: function() {
+	ill.printMode = function() {
 		// add printmode css
-		var inject = document.createElement("link");
+		let inject = document.createElement("link");
 		inject.setAttribute("rel", "stylesheet");
 		inject.setAttribute("href", "printmode.css");
 		document.head.appendChild(inject);
@@ -101,41 +105,44 @@ ill = {
 		[].forEach.call(document.querySelectorAll("*"), function(el) {
 			el.onclick = null;
 		});
-	}
-};
+	};
 
-window.onload = function() {
-	[].forEach.call(document.querySelectorAll(".record, .calculation"), function(el) {
-		el.onclick = function(event) {
-			if (el === event.target && event.offsetY < 60) {
-				ill.toggleRecord(el, event);
+
+	window.onload = function() {
+		[].forEach.call(document.querySelectorAll(".record, .calculation"), function(el) {
+			el.onclick = function(event) {
+				if (el === event.target && event.offsetY < 60) {
+					ill.toggleRecord(el, event);
+				} else {
+					ill.selectRecord(el, event);
+				}
+			};
+		});
+		[].forEach.call(document.querySelectorAll(".record > .label, .calculation > .label"), function(el) {
+			el.onclick = function(event) {
+				ill.toggleRecord(el.parentNode, event);
+			};
+		});
+		[].forEach.call(document.querySelectorAll(".record"), function(el) {
+			ill.addToggleAnnotations(el);
+		});
+		[].forEach.call(document.querySelectorAll("codesample"), function(el) {
+			ill.addShowCode(el);
+		});
+		ill.injectLabels();
+	};
+
+	window.onkeyup = function(e) {
+		let els;
+		if (e.keyCode === 27) {
+			els = document.querySelectorAll(".record.annotate");
+			if (els.length) {
+				[].forEach.call(els, function(rec) { rec.classList.remove("annotate"); });
 			} else {
-				ill.selectRecord(el, event);
+				ill.unselectAllRecords();
 			}
-		};
-	});
-	[].forEach.call(document.querySelectorAll(".record > .label, .calculation > .label"), function(el) {
-		el.onclick = function(event) {
-			ill.toggleRecord(el.parentNode, event);
-		};
-	});
-	[].forEach.call(document.querySelectorAll(".record"), function(el) {
-		ill.addToggleAnnotations(el);
-	});
-	[].forEach.call(document.querySelectorAll("codesample"), function(el) {
-		ill.addShowCode(el);
-	});
-	ill.injectLabels();
-};
-
-window.onkeyup = function(e) {
-	var els;
-	if (e.keyCode === 27) {
-		els = document.querySelectorAll(".record.annotate");
-		if (els.length) {
-			[].forEach.call(els, function(rec) { rec.classList.remove("annotate"); });
-		} else {
-			ill.unselectAllRecords();
 		}
-	}
-};
+	};
+
+	window.ill = ill;
+})();
