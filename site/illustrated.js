936f4b21cc84314916a822a00d6b2379289401e7
Michael Driscoll <michael@ulfheim.net>
2022-04-20 09:58:03 -0600

    Bold new graphics!

--- site/illustrated.js
+++ site/illustrated.js
@@ -1,17 +1,17 @@
-(function () {
+(() => {
 	"use strict";
 
 	let ill = {};
 
 	// viewports etc
 
-	ill.elementIsVisible = function(el) {
+	ill.elementIsVisible = (el) => {
 		let rect = el.getBoundingClientRect(),
 			viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
 		return !(rect.bottom < 0 || rect.top - viewHeight >= 0);
 	};
 
-	ill.ensureElementInView = function(el) {
+	ill.ensureElementInView = (el) => {
 		if (!ill.elementIsVisible(el)) {
 			el.scrollIntoView({behavior: "smooth"});
 		}
@@ -19,14 +19,14 @@
 
 	// events
 
-	ill.unselectAllRecords = function() {
+	ill.unselectAllRecords = () => {
 		[].forEach.call(document.querySelectorAll(".illustrated .record.selected, .illustrated .calculation.selected"),
-		function(el) {
+		(el) => {
 			el.classList.remove("selected");
 		});
 	};
 
-	ill.toggleRecord = function(element, event) {
+	ill.toggleRecord = (element, event) => {
 		let selected = element.classList.contains("selected");
 		ill.unselectAllRecords();
 		if (!selected) {
@@ -38,47 +38,47 @@
 		ill.ensureElementInView(element);
 	};
 
-	ill.selectRecord = function(element, event) {
+	ill.selectRecord = (element, event) => {
 		ill.unselectAllRecords();
 		element.classList.add("selected");
 		ill.cancel(event);
 		ill.ensureElementInView(element);
 	};
 
-	ill.showCode = function(element, event) {
+	ill.showCode = (element, event) => {
 		element.parentNode.classList.add("show");
 		ill.cancel(event);
 	};
 
-	ill.closeAllCode = function() {
-		[].forEach.call(document.querySelectorAll("codesample.show"), function(el) {
+	ill.closeAllCode = () => {
+		[].forEach.call(document.querySelectorAll("codesample.show"), (el) => {
 			el.classList.remove("show");
 		});
 	};
 
-	ill.toggleAnnotate = function(el) {
+	ill.toggleAnnotate = (el) => {
 		el.classList.toggle("annotate");
 	};
 
-	ill.cancel = function(event) {
+	ill.cancel = (event) => {
 		if (event) { event.stopPropagation(); }
 	};
 
 	// injections
 
-	ill.addShowCode = function(el) {
+	ill.addShowCode = (el) => {
 		el.innerHTML = document.getElementById("showCodeTmpl").innerHTML + el.innerHTML;
 	};
 
-	ill.addToggleAnnotations = function(record) {
+	ill.addToggleAnnotations = (record) => {
 		let expl = record.querySelector(".rec-explanation"),
 			copy = document.getElementById("annotateTmpl").cloneNode(true);
 		expl.insertAdjacentElement("afterend", copy);
 	};
 
-	ill.injectLabels = function() {
+	ill.injectLabels = () => {
 		let els = document.querySelectorAll(".string > .explanation, .decryption > .explanation");
-		[].forEach.call(els, function(expl) {
+		[].forEach.call(els, (expl) => {
 			let label = expl.parentNode.querySelector(".label"),
 				h4 = document.createElement("h4");
 			h4.appendChild(document.createTextNode(label.textContent));
@@ -86,29 +86,29 @@
 		});
 	};
 
-	ill.printMode = function() {
+	ill.printMode = () => {
 		// add printmode css
 		let inject = document.createElement("link");
 		inject.setAttribute("rel", "stylesheet");
 		inject.setAttribute("href", "printmode.css");
 		document.head.appendChild(inject);
 		// open everything up
-		[].forEach.call(document.querySelectorAll(".record, .calculation"), function(el){
+		[].forEach.call(document.querySelectorAll(".record, .calculation"), (el) => {
 			el.classList.add("selected");
 			el.classList.add("annotate");
 		});
-		[].forEach.call(document.querySelectorAll("codesample"), function(el){
+		[].forEach.call(document.querySelectorAll("codesample"), (el) => {
 			el.classList.add("show");
 		});
-		[].forEach.call(document.querySelectorAll("*"), function(el) {
+		[].forEach.call(document.querySelectorAll("*"), (el) => {
 			el.onclick = null;
 		});
 	};
 
 
-	window.onload = function() {
-		[].forEach.call(document.querySelectorAll(".record, .calculation"), function(el) {
-			el.onclick = function(event) {
+	window.onload = () => {
+		[].forEach.call(document.querySelectorAll(".record, .calculation"), (el) => {
+			el.onclick = (event) => {
 				if (el === event.target && event.offsetY < 60) {
 					ill.toggleRecord(el, event);
 				} else {
@@ -116,26 +116,26 @@
 				}
 			};
 		});
-		[].forEach.call(document.querySelectorAll(".rec-label"), function(el) {
-			el.onclick = function(event) {
+		[].forEach.call(document.querySelectorAll(".rec-label"), (el) => {
+			el.onclick = (event) => {
 				ill.toggleRecord(el.parentNode, event);
 			};
 		});
-		[].forEach.call(document.querySelectorAll(".record"), function(el) {
+		[].forEach.call(document.querySelectorAll(".record"), (el) => {
 			ill.addToggleAnnotations(el);
 		});
-		[].forEach.call(document.querySelectorAll("codesample"), function(el) {
+		[].forEach.call(document.querySelectorAll("codesample"), (el) => {
 			ill.addShowCode(el);
 		});
 		ill.injectLabels();
 	};
 
-	window.onkeyup = function(e) {
+	window.onkeyup = (e) => {
 		let els;
 		if (e.keyCode === 27) {
 			els = document.querySelectorAll(".record.annotate");
 			if (els.length) {
-				[].forEach.call(els, function(rec) { rec.classList.remove("annotate"); });
+				[].forEach.call(els, (rec) => { rec.classList.remove("annotate"); });
 			} else {
 				ill.unselectAllRecords();
 			}
